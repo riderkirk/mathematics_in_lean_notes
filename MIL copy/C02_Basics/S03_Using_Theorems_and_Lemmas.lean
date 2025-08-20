b7bd35942ac8e0e2ac4919ba17c8db901d760901
Rider Kirkpatrick <73277718+RiderKirk@users.noreply.github.com>
2025-08-19 21:33:59 -0500

    delete files

--- MIL copy/C02_Basics/S03_Using_Theorems_and_Lemmas.lean
+++ MIL copy/C02_Basics/S03_Using_Theorems_and_Lemmas.lean
@@ -44,7 +44,9 @@ example (x : ℝ) : x ≤ x :=
 
 -- Try this.
 example (h₀ : a ≤ b) (h₁ : b < c) (h₂ : c ≤ d) (h₃ : d < e) : a < e := by
-  sorry
+  apply lt_of_le_of_lt h₀
+  apply lt_trans h₁
+  exact lt_of_le_of_lt h₂ h₃
 
 example (h₀ : a ≤ b) (h₁ : b < c) (h₂ : c ≤ d) (h₃ : d < e) : a < e := by
   linarith
@@ -86,21 +88,31 @@ example (h₀ : a ≤ b) (h₁ : c < d) : a + exp c + e < b + exp d + e := by
     apply exp_lt_exp.mpr h₁
   apply le_refl
 
-example (h₀ : d ≤ e) : c + exp (a + d) ≤ c + exp (a + e) := by sorry
+example (h₀ : d ≤ e) : c + exp (a + d) ≤ c + exp (a + e) := by
+  apply add_le_add_left
+  rw [exp_le_exp]
+  apply add_le_add_left h₀
 
 example : (0 : ℝ) < 1 := by norm_num
 
 example (h : a ≤ b) : log (1 + exp a) ≤ log (1 + exp b) := by
-  have h₀ : 0 < 1 + exp a := by sorry
+  have h₀ : 0 < 1 + exp a := by
+    apply add_pos
+    · norm_num
+    apply exp_pos
   apply log_le_log h₀
-  sorry
+  apply add_le_add_left
+  rw [exp_le_exp]
+  exact h
 
 example : 0 ≤ a ^ 2 := by
-  -- apply?
   exact sq_nonneg a
 
 example (h : a ≤ b) : c - exp b ≤ c - exp a := by
-  sorry
+  apply sub_le_sub
+  · apply le_refl
+  exact exp_le_exp.mpr h
+
 
 example : 2*a*b ≤ a^2 + b^2 := by
   have h : 0 ≤ a^2 - 2*a*b + b^2
@@ -121,7 +133,15 @@ example : 2*a*b ≤ a^2 + b^2 := by
   linarith
 
 example : |a*b| ≤ (a^2 + b^2)/2 := by
-  sorry
+  have h₁ : a*b ≤ (a^2 + b^2)/2
+  calc
+    a*b ≤ a^2 + b^2 := by ring
+  
+  have h₂ : -(a*b) ≤ (a^2 + b^2)/2
+  calc
+    
+
+  exact abs_le'.mpr ⟨h₁ , h₂⟩
 
 #check abs_le'.mpr
 
